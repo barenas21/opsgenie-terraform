e098930dbc86f292ab03e8674bd675e17c5b52f7
barenas21 <barenas21@gmail.com>
2020-11-25 13:15:05 -0800

    added-two-users

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 # Configure the Opsgenie Provider
 provider "opsgenie" {
-  api_key = "$OPSGENIE_API_KEY"
+  api_key = "c04b1ee8-0763-4f50-845d-0f444781e001"
   api_url = "api.opsgenie.com" 
 }
 
 # Create a user
-resource "opsgenie_user" "test" {
+resource "opsgenie_user" "fred_krueger" {
   username  = "fkrueger@domain.com"
   full_name = "Fred Krueger"
   role      = "User"
@@ -15,10 +15,31 @@ resource "opsgenie_user" "test" {
   skype_username = "skypename"
   user_address {
       country = "USA"
-      state = "Illinois"
-      city = "Elmwood"
-      line = "Line"
-      zipcode = "66666"
+      state = "Ohio"
+      city = "Springwood"
+      line = ""
+      zipcode = "45505"
+  }
+  user_details = {
+    key1 = "val1,val2"
+    key2 = "val3,val4"
+  }
+}
+
+resource "opsgenie_user" "jason_voorhees" {
+  username  = "jvoorhees@domain.com"
+  full_name = "Jason Voorhees"
+  role      = "User"
+  locale    = "en_US"
+  timezone  = "America/New_York"
+  tags = ["sre", "opsgenie"]
+  skype_username = "skypename"
+  user_address {
+      country = "USA"
+      state = "New Jersey"
+      city = "Cunningham"
+      line = ""
+      zipcode = ""
   }
   user_details = {
     key1 = "val1,val2"
